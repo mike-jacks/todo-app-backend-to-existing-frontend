0e5affbfb24cfe1c3592f90bb8eaa235de067475
Mike Jacks <mjacks4816@xmail.dixietech.edu>
2024-04-04 23:44:42 -0600

    testing alembic

--- backend/models.py
+++ backend/models.py
@@ -2,11 +2,14 @@ from uuid import UUID
 
 from pydantic import BaseModel
 from sqlmodel import SQLModel, Field
+import sqlmodel
+
+from sqlalchemy import Column, String
 
 class Todo(SQLModel, table=True):
     id: UUID | None = Field(default=None, primary_key=True)
     item: str
-    description: str | None = Field(default=None)
+    description: str = Field(default="Default", sa_column=Column(server_default="Default", nullable=False, type_=sqlmodel.String))
 
 class CreateTodoRequest(SQLModel):
     item: str
